aa9c925e42bcdf287a086108fd72308a3f9cd347
weaq <oooweaqooo@gmail.com>
2023-08-29 07:21:19 +0700

    score

--- functions.php
+++ functions.php
@@ -589,4 +589,13 @@ function submitsScoreForm($params)
 	//exit;
 }
 
-// shortcode show score
+//
+function buddhistCalendar($ymd) {
+	$ymd = explode("-", $ymd);
+	$thai_month = array('','มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม');
+	$y = $ymd[0]+543;
+	$m = $thai_month[intval($ymd[1])];
+	$d = intval($ymd[2]);
+	$output = "วันที่ " . $d . " เดือน " . $m . " พ.ศ. " . $y ;
+	return $output;
+  }
